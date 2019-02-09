91f89e7fd06e0634a8b4dc6cf0ddc631be89f434
Chandrapurnima Bhatnagar <cpb.95.2012@gmail.com>
2019-02-09 09:34:31 +0530

    Movie Data Updated

--- src/main/java/com/jm/MovieData.java
+++ src/main/java/com/jm/MovieData.java
@@ -1,47 +1,97 @@
 package com.jm;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+
+import com.google.gson.*;
 
 public class MovieData {
 
-    static String   inputUrl="https://jsonmock.hackerrank.com/api/movies/search/?Title=";
+    static String inputUrl = "https://jsonmock.hackerrank.com/api/movies/search/?Title=";
+    static List<String> list = new ArrayList<>();
 
 
     public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System. in);
+        Scanner in = new Scanner(System.in);
         String substr = in.nextLine();
         MovieData movieData = new MovieData();
         StringBuilder sb = new StringBuilder();
-        sb.append(inputUrl);
-        sb.append(substr);
+        sb.append(inputUrl + substr);
+
         String requestURL = sb.toString();
-       movieData.getMovieTitles(requestURL);
+        movieData.getMovieTitles(requestURL);
+
+        Collections.sort(list);
+        String[] title = new String[list.size()];
+        list.toArray(title);
+        for (String s : title
+                ) {
+            System.out.println(s);
+
+        }
+
 
     }
 
-    static void getMovieTitles( String url) throws IOException {
-
-        System.out.println("indide get Movie Title");
-        System.out.println(httpGet(url));
-
-
-
-
-    }
-
-
-    static String httpGet(String url) throws IOException {
+    static void httpRequest(String url) throws IOException {
 
         URL urlobj = new URL(url);
-        HttpURLConnection httpURLConnection = (HttpURLConnection)urlobj.openConnection();
-         httpURLConnection.setRequestMethod("GET");
-       return String.valueOf(httpURLConnection.getResponseCode());
+        HttpURLConnection httpURLConnection = (HttpURLConnection) urlobj.openConnection();
+        httpURLConnection.setRequestMethod("GET");
+        if (httpURLConnection.getResponseCode() == 200) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            String response = br.readLine();
+            JsonElement jelement = new JsonParser().parse(response);
+            JsonObject jsonObject = jelement.getAsJsonObject();
+            JsonArray dataarray = jsonObject.getAsJsonArray("data");
+            System.out.println(dataarray.size());
 
 
+            for (JsonElement obj :
+                    dataarray) {
+                JsonObject jsonObject1 = obj.getAsJsonObject();
+
+                list.add(jsonObject1.get("Title").toString());
 
 
- }
+            }
+
+
+        }
+
+
+    }
+
+
+    static void getMovieTitles(String url) throws IOException {
+
+        URL urlobj = new URL(url);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) urlobj.openConnection();
+        httpURLConnection.setRequestMethod("GET");
+        if (httpURLConnection.getResponseCode() == 200) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            String response = br.readLine();
+            JsonElement jelement = new JsonParser().parse(response);
+            JsonObject jsonObject = jelement.getAsJsonObject();
+            int total_pages = Integer.parseInt(String.valueOf(jsonObject.get("total_pages")));
+            int per_page = Integer.parseInt(String.valueOf(jsonObject.get("per_page")));
+
+            while (total_pages != 0) {
+
+                String requesturl = url.concat("&page=" + total_pages);
+                httpRequest(requesturl);
+
+                total_pages--;
+            }
+
+        }
+
+    }
 }
